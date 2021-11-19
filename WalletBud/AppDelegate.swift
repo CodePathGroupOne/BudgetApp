36164a46c8c459e0ab3c19dead9c3b55bf0520ae
Ryan Johnson <rnjohnson18@users.noreply.github.com>
2021-11-19 03:19:35 -0600

    Added launch screen, made the login screen nicer, and additional view controllers

--- WalletBud/AppDelegate.swift
+++ WalletBud/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "wGsjfFVuV72zh09EBtISlyR7Vqy3V44fek7Us6ls"
+            $0.clientKey = "tA8TZQagClarZSYJ4ZeXIRUW7U2yRSwJdgcASiZ6"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
