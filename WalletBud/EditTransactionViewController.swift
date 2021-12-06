08410c384c999ef069e1195e381a18a2468b7f01
Sohil Shrestha <sohilshrestha@Sohils-MBP.lan>
2021-12-05 22:23:55 -0600

    User can update transactions

---
+++ WalletBud/EditTransactionViewController.swift
@@ -0,0 +1,111 @@
+//
+//  EditTransactionViewController.swift
+//  WalletBud
+//
+//  Created by Sohil Shrestha on 12/5/21.
+//
+
+import UIKit
+import Parse
+import DropDown
+
+class EditTransactionViewController: UIViewController {
+    @IBOutlet weak var vendorField: UITextField!
+    @IBOutlet weak var date: UIDatePicker!
+    @IBOutlet weak var Amount: UITextField!
+    @IBOutlet weak var categoryView: UIView!
+    
+    @IBOutlet weak var dropdownLabel: UILabel!
+    
+    var hashtagobject = [PFObject]()
+    let categoriesdropDown =  DropDown()
+    var categoriesArray = [
+        "Loading","Loading",
+        "Loading","Loading",
+        "Loading","Loading",
+        "Loading","Loading",
+        "Loading","Loading",
+    ]
+    var hashtagselectedindex = Int()
+    
+    var transaction : PFObject!
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Common_hashtags")
+        query.findObjectsInBackground { (hashtags,error) in
+            if hashtags != nil {
+                
+                self.hashtagobject = hashtags!
+                for i in 0...self.hashtagobject.count-1{
+                    self.categoriesArray[i] = self.hashtagobject[i]["Hashtag"] as! String
+                    if(self.hashtagobject[i].objectId == (self.transaction["hashTag"] as! PFObject).objectId){
+                        self.hashtagselectedindex = i
+                        self.dropdownLabel.text = self.hashtagobject[i]["Hashtag"] as! String
+                    }
+                }
+                self.categoriesdropDown.dataSource = self.categoriesArray
+                
+                self.categoryView.reloadInputViews()
+            }
+            
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(transaction)
+        
+        Amount.text = String(describing: transaction["Amount"]!)
+        vendorField.text = transaction["Transaction_vendor"] as! String
+        date.date = transaction["Transaction_date"] as! Date
+        
+        
+        categoriesdropDown.anchorView = categoryView
+        categoriesdropDown.dataSource = categoriesArray
+        categoriesdropDown.bottomOffset = CGPoint(x: 0, y:(categoriesdropDown.anchorView?.plainView.bounds.height)!)
+        categoriesdropDown.topOffset = CGPoint(x: 0, y:-(categoriesdropDown.anchorView?.plainView.bounds.height)!)
+        categoriesdropDown.direction = .bottom
+        categoriesdropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            print("Selected item: \(item) at index: \(index)")
+            dropdownLabel.text = categoriesArray[index]
+            hashtagselectedindex = index
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onDropDown(_ sender: Any) {
+        categoriesdropDown.show()
+    }
+    
+    @IBAction func updateTransaction(_ sender: Any) {
+        let query = PFQuery(className:"Transactions")
+        query.getObjectInBackground(withId: transaction.objectId as! String) { (updatetransaction: PFObject?, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let updatetransaction = updatetransaction {
+                updatetransaction["Amount"] = Double(self.Amount.text!)
+                updatetransaction["Transaction_vendor"] = self.vendorField.text!
+                updatetransaction["Transaction_date"] = self.date.date
+                updatetransaction["hashTag"] = self.hashtagobject[self.hashtagselectedindex]
+                // Update budget info
+                updatetransaction.saveInBackground()
+                //Show some error to user 
+                self.dismiss(animated: true, completion: nil)
+                
+            }
+        }
+    }
+        @IBAction func deleteTransaction(_ sender: Any) {
+        }
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
