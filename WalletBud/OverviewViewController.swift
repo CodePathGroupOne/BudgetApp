26032605f807f74c492f00ec1023a708610e2f2f
Sohil Shrestha <sohilshrestha@Sohils-MBP.lan>
2021-12-06 11:54:02 -0600

    Revert "Update"
    This reverts commit 9277bdeee0dca2bf8f4329b1b3bab55559e33168.

--- WalletBud/OverviewViewController.swift
+++ WalletBud/OverviewViewController.swift
@@ -7,15 +7,9 @@
 
 import UIKit
 import Parse
-import Charts
 
-class OverviewViewController: UIViewController,ChartViewDelegate {
+class OverviewViewController: UIViewController {
     
-    var pieChart = PieChartView()
-    
-    @IBOutlet weak var topView: UIView!
-    
-    @IBOutlet weak var bottomView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,78 +25,8 @@ class OverviewViewController: UIViewController,ChartViewDelegate {
          */
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        pieChart.frame = CGRect(x:0,y:0,width:
-                                self.bottomView.frame.size.width,
-                                height: self.bottomView.frame.size.width)
-        pieChart.center = self.bottomView.center
-        
-        pieChart.centerText = "This is the text\n 70%added"
-        view.addSubview(pieChart)
- 
-        var entries = [ChartDataEntry]()
-        
-        for x in 1..<3{
-            entries.append(ChartDataEntry(x:Double(x),y:Double(x)))
-        }
-                           
-        let set = PieChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.pastel()
-        let data = PieChartData(dataSet: set)
-        pieChart.data = data
-    }
-
-    func getCurrentMonthAndYear() -> Array<String>{
-        let now = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "LLLL"
-        let nameOfMonth = dateFormatter.string(from: now)
-        dateFormatter.dateFormat = "yyyy"
-        let yearString = dateFormatter.string(from: now)
-        
-        return [nameOfMonth,yearString]
-    }
-    func stringToDate(_ date: String) -> Date? {
-        let df = DateFormatter()
-        df.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale?
-        df.dateFormat = "LLLL yyyy"
-        df.date(from: date)
-        
-        return df.date(from: date)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        /*
-        // Get current budget
-        let currentUser:PFUser = PFUser.current()!
-        let query = PFQuery(className:"Budget")
-        let month_year = getCurrentMonthAndYear()
-        let year_month = month_year[0] + " " + month_year[1]
-        query.whereKey("User", equalTo: currentUser)
-        query.whereKey("Year_Month", equalTo: current_month)
-        
-        query.findObjectsInBackground { (hashtags,error) in
-            if hashtags != nil {
-                
-                self.hashtagobject = hashtags!
-                for i in 0...self.hashtagobject.count-1{
-                    self.categoriesArray[i] = self.hashtagobject[i]["Hashtag"] as! String
-                    if(self.hashtagobject[i].objectId == (self.transaction["hashTag"] as! PFObject).objectId){
-                        self.hashtagselectedindex = i
-                        self.dropdownLabel.text = self.hashtagobject[i]["Hashtag"] as! String
-                    }
-                }
-                self.categoriesdropDown.dataSource = self.categoriesArray
-                
-                self.categoryView.reloadInputViews()
-            }
-            
-        }*/
-    }
     @IBAction func onSignOut(_ sender: Any) {
-        
+        print("Click")
         PFUser.logOut()
                let main = UIStoryboard(name: "Main", bundle: nil)
                let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
