779da9c62a7d637f56730756575789aa7a7d12bc
Sohil Shrestha <sohilshrestha@Sohils-MBP.lan>
2021-12-08 20:35:39 -0600

    BarChart is also populated with correct data

--- WalletBud/OverviewViewController.swift
+++ WalletBud/OverviewViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Parse
 import Charts
-class OverviewViewController: UIViewController {
+class OverviewViewController: UIViewController,ChartViewDelegate {
     
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var topView: UIView!
@@ -16,7 +16,7 @@ class OverviewViewController: UIViewController {
     var barChart = BarChartView()
     var budget_by_hashtag = [String:Decimal]()
     var transaction_by_hashtag = [String:Decimal]()
-    
+    var hashtags = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +24,25 @@ class OverviewViewController: UIViewController {
         getCurrentMonthMetric(metric: "Budget")
         
         getCurrentMonthMetric(metric: "Transactions")
+        barChart.delegate = self
+        barChart.noDataText = "Fetching data from the server"
         
-        //createPieChart()
+        //Legend
+       let legend = barChart.legend
+       legend.enabled = true
+       legend.horizontalAlignment = .right
+       legend.verticalAlignment = .top
+       legend.orientation = .vertical
+       legend.drawInside = true
+       legend.yOffset = 10.0;
+       legend.xOffset = 10.0;
+       legend.yEntrySpace = 0.0;
         
-        //createBarChart()
-        
+        let yaxis = barChart.leftAxis
+                    yaxis.spaceTop = 0.35
+                    yaxis.axisMinimum = 0
+                    yaxis.drawGridLinesEnabled = false
+       
         // Do any additional setup after loading the view.
         /*let barAppearance = UINavigationBarAppearance()
          
@@ -55,6 +69,7 @@ class OverviewViewController: UIViewController {
         
         
         query.whereKey("User", equalTo: currentUser)
+        query.addAscendingOrder("Hashtag")
         if metric == "Budget"{
             query.includeKey("Hashtag")
             query.whereKey("Year_Month", equalTo: current_month)
@@ -68,7 +83,7 @@ class OverviewViewController: UIViewController {
             (result,error) in
             if error == nil {
                 let results = result!
-                
+               
                 for i in 0..<results.count{
                     if metric == "Budget"{
                         var temp = (results[i]["Hashtag"] as! PFObject)["Hashtag"] as! String
@@ -152,36 +167,85 @@ class OverviewViewController: UIViewController {
                                     self.bottomView.frame.size.width,
                                 height: self.bottomView.frame.size.height)
         barChart.center = self.bottomView.center
-        //barChart.legend.enabled = false
+
         //view.addSubview(barChart)
-        let legend = barChart.legend
+        barChart.noDataText = "Fetching data. Go to other tab"
         
         //Confuigure the axis
-        //let xAxis = barChart.xAxis
+        //barChart.xAxis.granularityEnabled = true
+        barChart.xAxis.granularity = 1
+        barChart.xAxis.centerAxisLabelsEnabled = true
+        //barChart.xAxis.valueFormatter = axisFormatDelegate
+      
+        barChart.xAxis.labelPosition = .bottom
+        barChart.xAxis.labelRotationAngle = 280
+        barChart.xAxis.drawGridLinesEnabled = true
+        barChart.xAxis.labelCount = 10
+        barChart.xAxis.granularityEnabled = true
+        barChart.rightAxis.enabled = false
         
-        var barentries = [BarChartDataEntry]()
         
-        for x in 1..<10{
-            barentries.append(BarChartDataEntry(x:Double(x),y:Double.random(in: 0...30)))
+        barChart.chartDescription?.enabled = false
+        //barChart.setVisibleXRangeMaximum(Double(5.0))
+ 
+       var bar_entries_budget = [BarChartDataEntry]()
+        var bar_entries_transaction = [BarChartDataEntry]()
+        var x = 0
+        self.hashtags = ["DUMMY"] // because formatter start at index 1. 
+        for (key, value) in self.budget_by_hashtag{
+            self.hashtags.append(key)
+            bar_entries_budget.append(BarChartDataEntry(x:Double(x),y:Double(truncating: value as NSNumber), data:key))
+            if self.transaction_by_hashtag[key] != nil{
+            
+                bar_entries_transaction.append(BarChartDataEntry(x:Double(x),y:Double(truncating: self.transaction_by_hashtag[key] as! NSNumber)))
+            }else{
+                bar_entries_transaction.append(BarChartDataEntry(x:Double(x),y:Double(0)))
+            }
+            
+            x+=1
+            
         }
+     
+        if self.budget_by_hashtag.count != 0{
+            print(self.hashtags)
+            print(bar_entries_budget)
+            print(bar_entries_transaction)
+            barChart.xAxis.valueFormatter =  IndexAxisValueFormatter(values: self.hashtags)
+            barChart.notifyDataSetChanged()
+        }
+        let bar_budget_set = BarChartDataSet(entries: bar_entries_budget,label: "Budget")
+        let bar_transaction_set = BarChartDataSet(entries: bar_entries_transaction,label: "Expense")
+        let dataSets: [BarChartDataSet] = [bar_budget_set,bar_transaction_set]
+        bar_budget_set.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+        //bar_budget_set.colors = ChartColorTemplates.pastel()
+        let bar_data = BarChartData(dataSets: dataSets)
+       
         
-        let barset = BarChartDataSet(entries: barentries,label: "Cost")
-        barset.colors = ChartColorTemplates.pastel()
-        let bardata = BarChartData(dataSet: barset)
-        barChart.data = bardata
+        let groupSpace = 0.3
+                let barSpace = 0.05
+                let barWidth = 0.3
+        
+        bar_data.barWidth = barWidth
+        barChart.xAxis.axisMinimum = Double(1.0)
+        let gg = bar_data.groupWidth(groupSpace: groupSpace, barSpace: barSpace)
+        barChart.xAxis.axisMaximum = Double(1.0) + gg * Double(10)
+        
+        
+        
+        bar_data.groupBars(fromX: Double(1.0), groupSpace: groupSpace, barSpace: barSpace)
+        
+        barChart.data = bar_data
+        
+        barChart.animate(xAxisDuration: 1.5, yAxisDuration: 1.5, easingOption: .linear)
         view.addSubview(barChart)
     }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        
-        
-        
-    }
+    
+ 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.createPieChart()
         self.createBarChart()
+        
     }
     
     func getCurrentandNextMonthAndYear() -> Array<String>{
